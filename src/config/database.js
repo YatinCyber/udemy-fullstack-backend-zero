b84d95f267cb4ea9dd22a10a12981ad342501298
YatinCyber <chabonglatui@gmail.com>
2023-05-09 18:35:45 +0700

    Test connect database

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2')
+require('dotenv').config()
+
+
+//test connection database
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+  })
+
+ module.exports = connection
--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
+const connection = require('./config/database')
 
 const app = express()    
+
 const port = process.env.PORT || 8080
 const hostname = process.env.HOST_NAME
 
@@ -12,6 +14,17 @@ configViewEngine(app)
 
 //khai bao routes
 app.use('/', webRoutes)
+
+ //simple query
+
+ connection.query(
+  'select * from Users u ',
+  function (err, results, fields) {
+    console.log(">>> RESULTS: " + JSON.stringify(results));
+    // console.log(">>> FIELDS: " + JSON.stringify(fields));
+
+  }
+)
 
 //app run
 app.listen(port, () => {
